d5a1ed72e7ad00c12bc24cbf8ac692cccb81cd20
MaxwellRibeiro <35998964+MaxwellRibeiro@users.noreply.github.com>
2019-11-07 10:11:41 -0300

    initial commit

---
+++ EstabelecimentoMRR/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Forms;
+
+namespace EstabelecimentoMRR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            //IDbConnection db = new MySqlConnection(ConfigurationManager.ConnectionStrings["Frota"].ConnectionString);
+            //db.Open();
+            //try
+            //{
+            //    var obj = db.Insert<T>(entity);
+            //    if (obj > 0)
+            //    {
+            //        return obj;
+            //    }
+            //    throw GaException.Exception(GaExceptionError.Insert);
+            //}
+            //catch (Exception x)
+            //{
+            //    throw GaException.Exception(x.Message);
+            //}
+            //finally
+            //{
+            //    db.Close();
+            //}
+        }
+    }
+}
