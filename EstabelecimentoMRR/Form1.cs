8dc731c4b4ce069dfb0f353679257cfd0cfd65ef
MaxwellRibeiro <35998964+MaxwellRibeiro@users.noreply.github.com>
2019-11-07 11:00:27 -0300

    ...

--- EstabelecimentoMRR/Form1.cs
+++ EstabelecimentoMRR/Form1.cs
@@ -11,25 +11,7 @@ namespace EstabelecimentoMRR
 
         private void Form1_Load(object sender, System.EventArgs e)
         {
-            //IDbConnection db = new MySqlConnection(ConfigurationManager.ConnectionStrings["Frota"].ConnectionString);
-            //db.Open();
-            //try
-            //{
-            //    var obj = db.Insert<T>(entity);
-            //    if (obj > 0)
-            //    {
-            //        return obj;
-            //    }
-            //    throw GaException.Exception(GaExceptionError.Insert);
-            //}
-            //catch (Exception x)
-            //{
-            //    throw GaException.Exception(x.Message);
-            //}
-            //finally
-            //{
-            //    db.Close();
-            //}
+            
         }
     }
 }
--- EstabelecimentoMRR/Repository/FluxoCaixaRep.cs
+++ EstabelecimentoMRR/Repository/FluxoCaixaRep.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Windows.Forms;
+using Dapper;
+using MySql.Data.MySqlClient;
 
 namespace EstabelecimentoMRR.Repository
 {
     public class FluxoCaixaRep
     {
+        public int Execute(string sql)
+        {
+            IDbConnection db = new MySqlConnection(ConfigurationManager.ConnectionStrings["local"].ConnectionString);
+            db.Open();
+            try
+            {
+                //Retorno: O número inteiro representa o número de linhas que foram afetadas pela sua consulta.
+                return db.Execute(sql);
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+                return 0;
+            }
+            finally
+            {
+                db.Close();
+            }
+        }
     }
 }
