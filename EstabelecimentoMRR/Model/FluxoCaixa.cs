daae72f57a28ba7b5f5e2cd7ad730c670201de22
MaxwellRibeiro <35998964+MaxwellRibeiro@users.noreply.github.com>
2019-11-07 10:20:00 -0300

    fluxocaixa

---
+++ EstabelecimentoMRR/Model/FluxoCaixa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Dapper.Contrib.Extensions;
+
+namespace EstabelecimentoMRR.Model
+{
+    [Table("fluxocaixa")]
+    public class FluxoCaixa
+    {
+        public int Id { get; set; }
+        public string Conta { get; set; }
+        public int TipoConta { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public decimal Valor { get; set; }
+        public int Status { get; set; }
+        public string Descricao { get; set; }
+    }
+}
